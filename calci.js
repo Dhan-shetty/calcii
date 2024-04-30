6d9c31cc2bdcbca743f57cef52b63df70f911015
Dhan-shetty <167894231+Dhan-shetty@users.noreply.github.com>
2024-04-30 21:01:03 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,15 @@
+function append(value){
+    document.getElementById('result').value += value;
+}
+
+function calculate(){
+    try{
+        document.getElementById('result').value = eval(document.getElementById('result').value);
+    } catch(error){
+        document.getElementById('result').value = 'Error!!!';
+    }
+}
+
+function clearC(){
+    document.getElementById('result').value = '';
+}
